3df9421336df876898f436e9f765a351ca40556b
pridi0t <yoeun18@gmail.com>
2024-02-18 20:46:40 +0900

    index.js에서 App.js를 사용하도록 수정

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import ProfilePage from "./component/page/ProfilePage";
 import BlogPage from "./component/page/BlogPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<p>메인화면</p>}/>
-        <Route path="/profile" element={<p>프로필화면</p>}/>
-        <Route path="/blog/*" element={<BlogPage />}/>
+          <Route index element={<ProfilePage />} />
+          <Route path="/profile" element={<ProfilePage />}/>
+          <Route path="/blog/*" element={<BlogPage />}/>
       </Routes>
     </BrowserRouter>
   );
